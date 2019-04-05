0c51d661d384d2a8887746cbfa7e0735ea4f5cba
Akhila <adsouz11@ford.com>
2019-04-05 16:46:52 +0100

    Base Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-seed-webpack
+
+Run the following in your terminal/console:
+
+```console
+npm run start:dev
+```
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+import "./App.css";
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <h1> Hello, World! ddd</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
